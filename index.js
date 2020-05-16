e60edf99c841c177e544ae2c22fa7eabccef9e48
mattted <medward4@gmail.com>
2020-05-16 14:18:46 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Add your functions here
+function map(arr, func) {
+  let newArr = []
+  arr.forEach( el => newArr.push(func(el)))
+  return newArr
+}
+
+function reduce(arr, func, start) {
+  let total = (!!start) ? start : arr[0]
+  let itarr = (!!start) ? arr.slice(0) : arr.slice(1)   
+
+  itarr.forEach( el => total = func(el, total))
+  return total
+}
