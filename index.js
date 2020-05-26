3b62203494b34e33d4e17b18fe71c391d5363444
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-26 12:49:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your functions here
+function map(array, fn){
+    let newArray = [];
+    array.forEach(index => {
+        newArray.push(fn(index))
+    });
+    return newArray;
+}
+
+function reduce(array, fn, startingPoint){
+    let accumulator;
+    let index;
+
+    if(!!startingPoint){
+        accumulator = startingPoint;
+        index = 0;
+    } else {
+        accumulator = fn(array[0],array[1]);
+        index = 2;
+    }
+    for(index; index< array.length; index++){
+        accumulator = fn(accumulator, array[index])
+    }
+    return accumulator;
+}
