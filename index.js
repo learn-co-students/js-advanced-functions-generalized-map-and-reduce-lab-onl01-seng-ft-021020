665dd254feea6e0a46f409f13b8435db302acb96
biagioo <mendolia2328@gmail.com>
2020-05-26 15:51:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Add your functions here
+
+// my own map-like methods
+//     map returns an array with all values made negative
+//       1) transforms correctly
+//       map returns an array with the original values
+//         2) transforms correctly
+//       map returns an array with the original values multiplied by 2
+//         3) transforms correctly
+//       map returns an array with the original values squared
+//         4) transforms correctly
+function map(arr, callback){
+    let newArr = [] 
+    arr.forEach(element => {
+       newArr.push(callback(element)) 
+    });
+    return newArr
+}
+//   my own reduce-like methods
+//     reduce returns a running total when not given a starting point
+//       5) reduces correctly
+//     reduce returns a running total when given a starting point
+//       6) reduces correctly
+//     reduce returns true when all values are true
+//       7) reduces correctly
+//     reduce returns false when any value is false
+//       8) reduces correctly
+//     reduce returns true when a true value is present
+//       9) reduces correctly
+//     reduce returns false when no true value is present
+//       10) reduces correctly
+
+function reduce(arr, callback, start){
+    let s = (!!start) ? start : arr[0]
+    let i = (!!start) ? 0 : 1
+
+    for (; i < arr.length; i++) {
+      s = callback(arr[i], s)
+    }
+    return s
+}
