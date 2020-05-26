613125240cdeb2b392b2a77898040f8888223e9c
github username <github email address>
2020-05-26 13:20:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(values, fun){
+    return values.map(fun)
+}
+
+function reduce(values, fun, startingPoint = 0){
+   
+    let result = values.reduce(fun)
+
+    if(startingPoint != 0){
+        result += startingPoint
+
+    }
+
+    return result
+
+}
