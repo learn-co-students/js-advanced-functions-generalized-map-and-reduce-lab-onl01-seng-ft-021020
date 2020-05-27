11cc83f384e1d5a04c6b907c9645dc2575b8e11d
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-05-27 18:48:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+
+function map(arr, callback){
+    let newArr = [] 
+    arr.forEach(element => {
+       newArr.push(callback(element)) 
+    });
+    return newArr
+}
+
+
+function reduce(arr, callback, start){
+    let s = (!!start) ? start : arr[0]
+    let i = (!!start) ? 0 : 1
+
+    for (; i < arr.length; i++) {
+      s = callback(arr[i], s)
+    }
+    return s
+}
