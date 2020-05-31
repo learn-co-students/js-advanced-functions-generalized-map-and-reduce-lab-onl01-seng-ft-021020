370f0303caf92e4ddc610587f5e1ef0ce2e1e9cc
taylorkraft <taylorkraft16@gmail.com>
2020-05-31 09:37:05 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your functions here
+function map(sourceArray, x) {
+  let m = []
+
+  for (let i = 0; i < sourceArray.length; i++) {
+    let e = sourceArray[i]
+    m.push(x(e))
+  }
+  return m;
+}
+
+function reduce(sourceArray, x, starting) {
+  let r = (!!starting) ? starting : sourceArray[0]
+  let i = (!!starting) ? 0 : 1
+
+  for (; i < sourceArray.length; i++) {
+    r = x(sourceArray[i], r)
+  }
+  return r;
+}
